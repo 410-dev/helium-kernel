93afd268c07162915b0fcb85789f2c6f38f4481f
410 <58301450+410-dev@users.noreply.github.com>
2023-05-05 02:33:38 -0400

    prep will not build dirs

--- prep.sh
+++ prep.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-# Create directories
-mkdir -p data/cache
-mkdir -p data/files
-mkdir -p data/commands
-
-# Copy defaults/config.json to data/config.json
 python3 kernel/bootprep.py --target ./ --registry defaults/registry-default
