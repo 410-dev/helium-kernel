891efa6ebe1d442cefcfbc4a7ca7ec6b67659c21
410 <58301450+410-dev@users.noreply.github.com>
2023-05-05 02:46:12 -0400

    Create DataBuild.py

---
+++ kernel/hooks/DataBuild.py
@@ -0,0 +1,24 @@
+import kernel.registry as Registry
+import json
+import os
+
+class DataBuild():
+    
+    def __init__(self, args):
+        self.args = args
+    
+    def exec(self):
+        masterList: str = Registry.read("SOFTWARE.Helium.Values.Data.Struct.MasterList")
+        masterList = json.loads(masterList)
+        masterList = masterList['data']
+        
+        for item in masterList:
+            path: str = Registry.read(f"SOFTWARE.Helium.Values.Data.Struct.{item}")
+            path: list = path.split("/")
+            builtSoFar: str = ""
+            for parents in path:
+                builtSoFar = os.path.join(builtSoFar, parents)
+                if not os.path.exists(builtSoFar):
+                    os.mkdir(builtSoFar)
+        
+        
