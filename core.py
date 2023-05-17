6c1ea5e39ca705139a7223893a371fd13ea86893
410 <58301450+410-dev@users.noreply.github.com>
2023-05-16 22:19:34 -0400

    Profile is always updated on boot

--- core.py
+++ core.py
@@ -1,8 +1,11 @@
 import kernel.procmgr as procmgr
 import kernel.argsParser as argsParser
 import kernel.hookmgr as hookmgr
+import kernel.registry as Registry
 import time
 from kernel.states import States
+
+Registry.build("defaults/registry-reserved", silent = True)
 
 States.setObj("Core.Cache.StartTime.Epoch", round(time.time()*1000))
 
