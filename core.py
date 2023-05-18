2873f9a4dd60adff3eeb0607c9b82c7dca270840
410 <58301450+410-dev@users.noreply.github.com>
2023-05-17 22:18:22 -0400

    Registry building updated

--- core.py
+++ core.py
@@ -3,15 +3,17 @@ import kernel.argsParser as argsParser
 import kernel.hookmgr as hookmgr
 import kernel.registry as Registry
 import time
+import os
 from kernel.states import States
 
-Registry.build("defaults/registry-reserved", silent = True)
+Registry.build("defaults/registry-default", os.path.join("./", "registry"), silent=True, overwrite=False)
+Registry.build("defaults/registry-enforced", os.path.join("./", "registry"), silent = True, overwrite=True)
 
 States.setObj("Core.Cache.StartTime.Epoch", round(time.time()*1000))
 
 hookmgr.runHooks([], kernelHooks = True)
 hookmgr.runHooks([], kernelHooks = False)
 
-
+Registry.build("defaults/registry-enforced", os.path.join("./", "registry"), silent = True, overwrite=True)
 
 procmgr.exec("system.py", "System", [])
