7dc40e617d3d81143f4ca1712b06eb21bf543ee0
410 <58301450+410-dev@users.noreply.github.com>
2023-05-18 02:07:37 -0400

    Update core.py

--- core.py
+++ core.py
@@ -4,12 +4,15 @@ import kernel.hookmgr as hookmgr
 import kernel.registry as Registry
 import time
 import os
-from kernel.states import States
+from kernel.states import IPCMemory
+
+IPCMemory.setObj("System.Cache.StartTime.Epoch", round(time.time()*1000), True, "1111")
+IPCMemory.setObj("System.Location.Root", os.path.abspath("./"), True, "1111")
 
 Registry.build("defaults/registry-default", os.path.join("./", "registry"), silent=True, overwrite=False)
 Registry.build("defaults/registry-enforced", os.path.join("./", "registry"), silent = True, overwrite=True)
 
-States.setObj("Core.Cache.StartTime.Epoch", round(time.time()*1000))
+
 
 hookmgr.runHooks([], kernelHooks = True)
 hookmgr.runHooks([], kernelHooks = False)
